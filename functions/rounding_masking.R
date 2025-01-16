9db29b272495dd888300a50de2c1192ce8d28f45
Carlos <85176234+CarlosPoses@users.noreply.github.com>
2025-01-16 13:49:22 +0100

    Update rounding_masking.R

--- functions/rounding_masking.R
+++ functions/rounding_masking.R
@@ -38,52 +38,65 @@ mask_count <- function(x, threshold = 5){
 
 ## output: character vector with counts below threshold masked + one interval masked value
 
-interval_mask <- function(x,threshold = 5){
-  naive_interval <- 1:(threshold - 1) # equivalent to <5
-  counts <- x
-  total <- sum(counts)
-  is_masked <- ifelse(counts <5, TRUE, FALSE)
-  n_masked <- sum(is_masked)
-
-  # choose larger value to be interval masked
-  is_possible_interval <- !is_masked
-  potential_interval <- sort(counts[is_possible_interval], decreasing = TRUE)
-  is_interval_masked <- potential_interval[1]
-  is_not_interval_masked <- potential_interval[-1] # store for interval mask bounds
+interval_mask <- function(counts, threshold = 5, output_warnings = TRUE){
+  naive_interval <- 1:(threshold - 1) # interval to mask individual counts enforced by daps
+  total <- sum(counts) 
+  naively_masked <- ifelse(counts <5, TRUE, FALSE) # logical, values that will be naively masked
+  n_masked <- sum(naively_masked) # number of masked values
+  not_naively_masked <- !naively_masked # logical, values that are not naively masked
+  # value that will be interval masked
+  interval_masked <- max(counts[not_naively_masked]) # value that will be interval masked
+  ## When two counts have the same value, potentially they both could be interval masked
+  ## Next three lines are necessary to select only one
+  interval_masked_index <- which(counts == interval_masked)[1] # index interval_masked_value (only first one)
+  interval_mask_logical <- rep(FALSE, length(counts)) 
+  interval_mask_logical[interval_masked_index] <- TRUE # Vector where the only TRUE corresponds to index of ONE interval masked value 
   
-  # calculate interval bounds
-  upper.int <- total - sum(is_not_interval_masked) - n_masked*1
-  lower.int <- total - sum(is_not_interval_masked) - n_masked*(threshold-1)
+  # Select not_masked_variables
+  not_masked_logical <- not_naively_masked & !interval_mask_logical
+  not_masked <- counts[not_masked_logical]
   
-  if(lower.int <5) {
-    excluded_int <- lower.int:(threshold-1)
-    
-    # generate all possible combinations of naively masked values
-    interval_combs <- do.call(expand.grid, replicate(n_masked, naive_interval, simplify = FALSE))
-    # check which possible combinations of naively masked values are not possible
-    interval_combs$sums <- rowSums(interval_combs)
-    excluded_values <- subset(interval_combs, interval_combs$sums %in% excluded_int)[, !names(interval_combs) == 'sums']
-    possible_values <- subset(interval_combs, !interval_combs$sums %in% excluded_int)[, !names(interval_combs) == 'sums']
-    perc_impossible <- paste0((nrow(excluded_values)/nrow(interval_combs))*100, '%')
-    
-    warning(paste0('This table may not be safe. Of all possible combinations of naively masked values, ', 
-                   perc_impossible, 'value combinations can be excluded. You can explore the excluded and the possible values
-                   in the output.'))
-    cat('Here are the combinations of masked values that are not possible')
-    print(excluded_values)
-    cat('Here are the combinations of unmasked values that are not possible')
-    print(possible_values)
-  }
+  # Create interval using formula in the slides
+  upper.int <- total - sum(not_masked) - n_masked*1
+  lower.int <- total - sum(not_masked) - n_masked*(threshold-1)
   
-  # interval mask one additional value
+  # Apply masking
+  ## For values below threshold, just mask
   masked_counts <- ifelse(counts <5, '[1-4]', counts)
-  index_interval_masked <- which(counts == is_interval_masked)[1]
-  masked_counts[index_interval_masked] <- paste0('[',lower.int, '-', upper.int, ']')
+  ## For interval masked values, substitute by interval
+  ## When there are twe counts that are equal to the count that should be interval masked,
+  ## only mask one of them
+  masked_counts[interval_masked_index] <-  paste0('[',lower.int, '-', upper.int, ']')
+  
+  # optional warnings 
+  if(output_warnings == TRUE) {
+    ## warning explicits which combinations of values are or are not possible
+    if(lower.int <5) {
+      excluded_int <- lower.int:(threshold-1)
+      interval_combs <- do.call(expand.grid, replicate(n_masked, naive_interval, simplify = FALSE))
+      
+      interval_combs$sums <- rowSums(interval_combs)
+      
+      ## WARNING: I THINK THIS IS THE OTHER WAY AROUND?
+      excluded_values <- subset(interval_combs, !interval_combs$sums %in% excluded_int)[, !names(interval_combs) == 'sums']
+      possible_values <- subset(interval_combs, interval_combs$sums %in% excluded_int)[, !names(interval_combs) == 'sums']
+      
+      
+      perc_impossible <- paste0((nrow(excluded_values)/nrow(interval_combs))*100, '%')
+      
+      warning(paste0('This table may not be safe. Of all possible combinations of naively masked values, ', 
+                     perc_impossible, ' value combinations can be excluded'))
+      cat('Here are the combinations of masked values that are not possible')
+      print(excluded_values)
+      cat('Here are the combinations of unmasked values that are possible')
+      print(possible_values)
+      
+    }
+  }
   
   return(masked_counts)
   
 }
-
 
 # overall function to mask baseline descriptives coming from DescriptivesTable
 mask_round_descriptives <- function(raw_table, number =5, nsmall =1){
