0805494dc2b5166328eb7778e199f261048c9ee6
Carlos <85176234+CarlosPoses@users.noreply.github.com>
2025-01-16 16:54:36 +0100

    add a dev-wrapper file
    Only for development purposes

---
+++ dev-wrapper.R
@@ -0,0 +1,52 @@
+# Specify the file path
+file_path <- "example_script.R"
+
+# Read the first 40 lines of the file
+lines_to_run <- readLines(file_path, n = 69)
+
+# Evaluate the lines
+eval(parse(text = lines_to_run))
+
+library(dplyr)
+
+interval_masking_wrapper <- function(tableout, threshold = 5, output_warnings = FALSE){
+# 1. Filter applicable variable types
+tableout_cat <- tableout %>% 
+  as.data.frame() %>% 
+  filter(type != 'NUM1',
+         type != 'TF', # WARNING: WILL need to be handled
+         var != 'Total')
+
+# 2. Obtain unique categories
+categories <- unique(tableout_cat$var)
+
+# This part will be iterated later on
+for(i in seq_along(categories)){
+  cat <- categories[i]
+  print(i)
+  # Filter only one category
+  tableout_reduced <- tableout_cat[tableout_cat$var == cat,]
+
+  # Extract control and exposed vector
+  v1_control <- as.numeric(tableout_reduced$V1_CONTROL)
+  v1_exposed <- as.numeric(tableout_reduced$V1_EXPOSED)
+
+  # Apply interval mask function
+  mask_v1_control <- interval_mask(v1_control, threshold = threshold, output_warnings = output_warnings)
+  mask_v1_exposed <- interval_mask(v1_exposed, threshold = threshold, output_warnings = output_warnings)
+
+  # Get row indices of first and last ocurrence
+  first_index <- which(tableout$var == cat)[1]
+  last_index <- tail(which(tableout$var == cat), n = 1)
+  
+  # Replace appropriate rows and columns, based on index and column name
+  tableout[first_index:last_index, 'V1_CONTROL'] <- mask_v1_control
+  tableout[first_index:last_index, 'V1_EXPOSED'] <- mask_v1_exposed
+  }
+  
+return(tableout)
+
+}
+
+
+tableout_masked <- interval_masking_wrapper(tableout)
